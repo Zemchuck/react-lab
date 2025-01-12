d0068dd995b4ae05d0568ec63a0430124b45a3a1
Konrad Zemczak <zemczakk@gmail.com>
2025-01-12 15:48:54 +0100

    tak

--- src/App.js
+++ src/App.js
@@ -1,24 +1,45 @@
-import logo from './logo.svg';
 import './App.css';
+import {useState} from "react";
+import "milligram"
 
 function App() {
+    const [title, setTitle]= useState('Wall-e');
+    let message;
+    const movies = [
+        {title: "Wall-E"},
+        {title: "Pulp Fiction"},
+        {title: "Matrix"},
+        {title: "1670"},];
+    function handleChange(event){
+        setTitle(event.target.value);
+        if (title.length < 5) {
+            message = "Tytuł jest za krótki. Nagrywają takie filmy?";}
+        else if (title.length < 15) {
+            message = "Tytuł jest ekstra, w sam raz na plakat przed kinem!";}
+        else {
+            message = "Tytuł jest za długi, nikt tego nie zapamięta.";}
+    }
+    function showMovie(){
+        alert(title);
+    }
+    function displayMovie(){
+    }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div className="App">
+          <h1>My favorite movie to watch</h1>
+          <div>
+              <h2>Titles:</h2>
+              <ul>
+                  {movies.map((movie) => <li key={movie.title}>{movie.title}</li>)}
+              </ul>
+          </div>
+          <div>
+              <h2>My favorite movie for today is: {title}</h2>
+              {title.length > 0 && <div>{message}</div>}
+              <input type="text" value={title} onChange={handleChange}/>
+              <button onClick={showMovie}> Show Movie title </button>
+          </div>
+      </div>
   );
 }
 
